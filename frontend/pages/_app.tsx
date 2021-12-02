8bc2ddab40844a377cc3fdab2d4c4a49867cb2db
Dusan Mijatovic (PC2020) <d.mijatovic@dv4all.nl>
2021-12-02 09:35:38 +0100

    feat: add tiny progressbar on page load (SSR)

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,3 +1,5 @@
+import {useEffect, useState} from 'react'
+import router from 'next/router'
 import { SessionProvider } from "next-auth/react"
 import Head from 'next/head';
 import {AppProps} from 'next/app'
@@ -6,6 +8,10 @@ import CssBaseline from '@mui/material/CssBaseline';
 import {CacheProvider, EmotionCache} from '@emotion/react';
 import {rsdTheme} from '../styles/rsdTheme';
 import createEmotionCache from '../styles/createEmotionCache';
+
+// show loading bar at the top of the screen
+import nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 // global CSS
 import '../styles/global.css'
@@ -18,9 +24,28 @@ export interface MuiAppProps extends AppProps{
   emotionCache: EmotionCache
 }
 
+// define npgrogres setup, no spinner
+// just a tiny bar at the top of the screen
+nprogress.configure({showSpinner:false})
+
 export default function RsdApp(props:MuiAppProps) {
   const {Component, emotionCache = clientSideEmotionCache, pageProps} = props;
   const {pageProps:{session}} = props
+
+  useEffect(()=>{
+    router.events.on("routeChangeStart", ()=>{
+      // console.log("routeChangeStart...")
+      nprogress.start()
+    });
+    router.events.on("routeChangeComplete", ()=>{
+      // console.log("routeChangeComplete...")
+      nprogress.done()
+    });
+    router.events.on("routeChangeError", ()=>{
+      // console.log("routeChangeError...")
+      nprogress.done()
+    })
+  },[])
 
   return (
     <CacheProvider value={emotionCache}>
@@ -29,7 +54,7 @@ export default function RsdApp(props:MuiAppProps) {
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={rsdTheme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        {/* CssBaseline from MUI-5*/}
         <CssBaseline />
         <SessionProvider session={session}>
           <Component {...pageProps} />
