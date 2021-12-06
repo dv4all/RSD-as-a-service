ff8807fc4d7015ac729359eb9a14fec8d5d4a6c5
Jesús García <ctw@ctwhome.com>
2021-12-06 15:42:44 +0100

    minor: update dependencies

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   mode: 'jit',
   purge: [
-    '{layout,pages,components}/**/*.{j,t}s*'
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js,ts,jsx,tsx}'
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
